17ac30fe169fb87985bc69385a287c2bb086f70f
მაცი <93665709+maci03@users.noreply.github.com>
2023-10-30 17:09:16 +0400

    import navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Navbar } from './Components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
-    
+    <Navbar />
     </div>
   );
 }

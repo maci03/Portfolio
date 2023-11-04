479cef810afd4a30b34a338c2633285b33f54e9f
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-04 17:36:43 +0400

    contact component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,3 +3,17 @@ import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+
+
+export const Contact = () => {
+    const formInitialDetails = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      message: ''
+    }
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('Send');
+    const [status, setStatus] = useState({});
+  

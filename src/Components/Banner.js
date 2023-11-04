d47e8f669b8a11dcb1371fb325d8253e2d29a634
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-04 17:30:01 +0400

    tick

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -21,3 +21,27 @@ export const Banner = () => {
 
     return () => { clearInterval(ticker) };
   }, [text]);
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
